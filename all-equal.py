fbeee7d495dc3708542f61a928d039c0b32c36f1
Carlos Oz <carlos.oz.fi@gmail.com>
2023-10-08 18:53:13 -0600

    updt

--- all-equal.py
+++ all-equal.py
@@ -1,6 +1,9 @@
 def all_equal(my_list):
+    if not my_list:
+        return True
 
-    return False
+    first_element = my_list[0]
+    return all(element == first_element for element in my_list)
 
 
-print(all_equal([]))
+print(all_equal([1, 1, 2]))
